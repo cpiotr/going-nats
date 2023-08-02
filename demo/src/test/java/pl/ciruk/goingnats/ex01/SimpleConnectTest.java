f0e22427a0ee7fad730fe89c51cfc0da5e86a6e1
c.piotre <c.piotre@gmail.com>
2023-08-02 22:25:46 +0200

    Add connection examples

---
+++ demo/src/test/java/pl/ciruk/goingnats/ex01/SimpleConnectTest.java
@@ -0,0 +1,31 @@
+package pl.ciruk.goingnats.ex01;
+
+import io.nats.client.Nats;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+
+@Testcontainers
+public class SimpleConnectTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    @Container
+    private static final GenericContainer<?> NATS_SERVER = new GenericContainer<>("nats:2.6.5")
+            .withExposedPorts(4222, 8222)
+            .withReuse(true)
+            .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(30)));
+
+    @Test
+    void shouldConnectToNats() throws IOException, InterruptedException {
+        final var connection = Nats.connect("nats://%s:%s".formatted(NATS_SERVER.getHost(), NATS_SERVER.getMappedPort(4222)));
+        LOGGER.info("http://{}:{}/connz", NATS_SERVER.getHost(), NATS_SERVER.getMappedPort(8222));
+        Thread.sleep(Duration.ofHours(1));
+    }
+}
---
+++ demo/src/test/java/pl/ciruk/goingnats/ex02/OptionsTest.java
@@ -0,0 +1,65 @@
+package pl.ciruk.goingnats.ex02;
+
+import io.nats.client.Connection;
+import io.nats.client.ConnectionListener;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.net.InetAddress;
+import java.time.Duration;
+
+public class OptionsTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    void shouldConnectIdentifyingWithConnectionName() throws IOException, InterruptedException {
+        final var options = Options.builder()
+                .connectionName("client-1@" + InetAddress.getLocalHost().getHostName())
+                .server("nats://localhost:4222")
+                .build();
+        final var connection = Nats.connect(options);
+        Thread.sleep(Duration.ofHours(1));
+    }
+
+    @Test
+    void shouldHandleConnectionEvents() throws IOException, InterruptedException {
+        final var options = Options.builder()
+                .connectionName("client-2@" + InetAddress.getLocalHost().getHostName())
+                .server("nats://localhost:4222")
+                .connectionListener(new ConnectionListener() {
+                    @Override
+                    public void connectionEvent(Connection connection, Events events) {
+                        LOGGER.info("Events: {}", events);
+                    }
+                })
+                .build();
+        final var connection = Nats.connect(options);
+        Thread.sleep(Duration.ofHours(1));
+    }
+
+    @Test
+    void shouldHandleConnectionEventsButSloooowly() throws IOException, InterruptedException {
+        final var options = Options.builder()
+                .connectionName("client-2@" + InetAddress.getLocalHost().getHostName())
+                .server("nats://localhost:4222")
+                .connectionListener(new ConnectionListener() {
+                    @Override
+                    public void connectionEvent(Connection connection, Events events) {
+                        LOGGER.info("Events: {}", events);
+                        try {
+                            Thread.sleep(Duration.ofSeconds(10));
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                })
+                .build();
+        final var connection = Nats.connect(options);
+        Thread.sleep(Duration.ofHours(1));
+    }
+}
