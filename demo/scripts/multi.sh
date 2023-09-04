15e06c423f32556949cbd1515f643355316b91b4
c.piotre <c.piotre@gmail.com>
2023-09-04 23:12:02 +0200

    Add third node to cluster demo

--- demo/scripts/multi.sh
+++ demo/scripts/multi.sh
@@ -3,3 +3,6 @@ docker run --rm --name=nats1 -p 4222:4222 -p 6222:6222 -p 8222:8222 -v "/home/pi
 
 # Second
 docker run --rm --name=nats2 --link nats1 -p 4223:4222 -p 6223:6222 -p 8223:8222 -v "/home/piotr/git/going-nats/conf/":"/etc/nats/" nats -c /etc/nats/multi2.conf
+
+# Third
+docker run --rm --name=nats3 --link nats2 -p 4224:4222 -p 6224:6222 -p 8224:8222 -v "/home/piotr/git/going-nats/conf/":"/etc/nats/" nats -c /etc/nats/multi3.conf
